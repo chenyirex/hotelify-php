7d7381e5d47201aff453049ae1004694ce073230
chenyirex <chenyirex@outlook.com>
2018-11-11 15:39:32 -0800

    addressed comments

--- public/index.php
+++ public/index.php
@@ -18,8 +18,9 @@ $app->options('/{routes:.+}', function ($request, $response, $args) {
     return $response;
 });
 $app->add(function ($req, $res, $next) {
-    $response = $next($req, $res)->withHeader('Content-type', 'application/json');
+    $response = $next($req, $res);
     return $response
+        ->withHeader('Content-type', 'application/json')
         ->withHeader('Access-Control-Allow-Origin', '*')
         ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
         ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
