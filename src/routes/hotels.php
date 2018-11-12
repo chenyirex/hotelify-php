012a67794938592ebc689dd7754390d420e20c64
suiyoubi <mike.tang96@gmail.com>
2018-11-11 18:07:53 -0800

    constant

--- src/routes/hotels.php
+++ src/routes/hotels.php
@@ -9,6 +9,9 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
+$address_columns = 'a.id as address_id, a.country, a.province, a.city, a.postal_code, a.street';
+$hotel_columns = 'h.id as hotel_id, h.brand_name, h.branch_name, h.property_class, h.description';
+
 // Get All hotels
 $app->get('/api/hotels', function (Request $request, Response $response) {
 
@@ -36,17 +39,20 @@ $app->get('/api/hotels/search', function (Request $request, Response $response) 
     $brandName = $request->getParam('brandName');
     $branchName = $request->getParam('branchName');
 
+    $hotel_columns = $GLOBALS['hotel_columns'];
+    $address_columns = $GLOBALS['address_columns'];
+
     if ($brandName && $branchName) {
-        $sql = "SELECT h.id as hotel_id, a.id as address_id, h.*, a.* " .
+        $sql = "SELECT $hotel_columns, $address_columns " .
             "FROM hotel h, address a " .
             "WHERE h.brand_name = '$brandName' " .
             "AND h.branch_name = '$branchName' ";
     } else if ($branchName && !$brandName) {
-        $sql = "SELECT h.id as hotel_id, a.id as address_id, h.*, a.* " .
+        $sql = "SELECT $hotel_columns, $address_columns " .
             "FROM hotel h, address a " .
             "WHERE h.branch_name = '$branchName' ";
     } else if (!$branchName && $brandName) {
-        $sql = "SELECT h.id as hotel_id, a.id as address_id, h.*, a.* " .
+        $sql = "SELECT $hotel_columns, $address_columns " .
             "FROM hotel h, address a " .
             "WHERE h.brand_name = '$brandName' ";
     } else {
