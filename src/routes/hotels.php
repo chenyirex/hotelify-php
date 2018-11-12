ff6ea7294f406307743d7b6e2e7cfb12f4188262
suiyoubi <mike.tang96@gmail.com>
2018-11-11 17:22:05 -0800

    Add search by branch/brand name function

--- src/routes/hotels.php
+++ src/routes/hotels.php
@@ -31,5 +31,43 @@ $app->get('/api/hotels', function (Request $request, Response $response) {
 });
 
 // search hotels by branch name and brand name
-$app->post('/api/hotels/search', function (Request $request, Response $response) {
+$app->get('/api/hotels/search', function (Request $request, Response $response) {
+
+    $brandName = $request->getParam('brandName');
+    $branchName = $request->getParam('branchName');
+
+    if($brandName && $branchName) {
+        $sql = "SELECT h.id as hotel_id, a.id as address_id, h.*, a.* ".
+            "FROM hotel h, address a ".
+            "WHERE h.brand_name = '$brandName' ".
+            "AND h.branch_name = '$branchName' ";
+    } else if($branchName && !$brandName){
+        $sql = "SELECT h.id as hotel_id, a.id as address_id, h.*, a.* ".
+            "FROM hotel h, address a ".
+            "WHERE h.branch_name = '$branchName' ";
+    } else if(!$branchName && $brandName) {
+        $sql = "SELECT h.id as hotel_id, a.id as address_id, h.*, a.* ".
+            "FROM hotel h, address a ".
+            "WHERE h.brand_name = '$brandName' ";
+    } else {
+        // no params, bad request
+        return $response->withStatus(400);
+    }
+    try {
+        // Get DB Object
+        $db = new db();
+        // Connect
+        $db = $db->connect();
+        $stmt = $db->query($sql);
+        $hotels = $stmt->fetchAll(PDO::FETCH_OBJ);
+        $db = null;
+
+        $response->write(json_encode($hotels));
+        return $response->withStatus(200);
+    } catch (PDOException $e) {
+        $response->write($e);
+        return $response->withStatus(500);
+    }
+    echo json_encode($brandName);
+
 });
